1eac42070467e9e36b35650fcadaaa549034deb1
oksana-1 <4438488@gmail.com>
2021-11-29 21:56:24 +0200

    Make filter working

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -3,19 +3,22 @@ import Card from "../UI/Card";
 import "./ExpenseList.css"
 import ExpensesFilter from "./ExpensesFilter";
 
-function ExpenseList(props) {
+function ExpenseList({ expenses, selectedYear, setYear }) {
+	const filteredExpenses = expenses.filter((expense) => expense.date.getFullYear() === parseInt(selectedYear));
 	return (
 		<Card className="expenses">
-			<ExpensesFilter onFilterSelect={props.setYear} selectedYear={props.selectedYear} />
-			{ props.expenses.map((expense) => {
-				return (
-					<ExpenseItem
-						key={expense.id}
-						date={expense.date}
-						title={expense.title}
-						amount={expense.amount}/>
-				);
-			}) }
+			<ExpensesFilter onFilterSelect={setYear} selectedYear={selectedYear} />
+			{
+				filteredExpenses.map((expense) => {
+					return (
+						<ExpenseItem
+							key={expense.id}
+							date={expense.date}
+							title={expense.title}
+							amount={expense.amount}/>
+					);
+				})
+			}
 		</Card>
 	);
 }
